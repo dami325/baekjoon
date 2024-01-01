114561582bd19a617aa21cd723fc729703dd5f4b
dami325 <wnekfa1004@naver.com>
2024-01-01 17:40:43 +0900

    배열

--- src/main/java/com/study/baekjoon/step3/별찍기/Main.java
+++ src/main/java/com/study/baekjoon/step3/별찍기/Main.java
@@ -5,15 +5,19 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        for (int i = 0; i < n; i++) {
-            String result = "";
-            for (int j = 0; j <= i; j++) {
-                result += "*";
-                if (j == i) {
-                    System.out.println(result);
-                }
+        int line = sc.nextInt();
+        String result = "";
+        for (int i = 1; i <= line; i++) {
+            int space = line - i;
+            for (int draw = 1; draw <= space; draw++) {
+                result += " ";
             }
+
+            for (int star = 1; star <= i; star++) {
+                result += "*";
+            }
+            result += "\n";
         }
+        System.out.println(result);
     }
 }
