9606fa2c24d38ec9e1801ba484150bf73b7197db
Judalm Park (dami) <wnekfa1004@naver.com>
2024-01-03 09:52:22 +0900

    문자열

---
+++ src/main/java/com/study/baekjoon/문자열/단어길이재기/Main.java
@@ -0,0 +1,14 @@
+package com.study.baekjoon.문자열.단어길이재기;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        String world = sc.next();
+
+        System.out.println(world.length());
+    }
+}
