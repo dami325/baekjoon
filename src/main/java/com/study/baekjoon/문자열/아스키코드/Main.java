a66ce563ac7d2bc5fd5e09bd8002e1abec5e9b18
Judalm Park (dami) <wnekfa1004@naver.com>
2024-01-06 02:51:46 +0900

    문자열

---
+++ src/main/java/com/study/baekjoon/문자열/아스키코드/Main.java
@@ -0,0 +1,13 @@
+package com.study.baekjoon.문자열.아스키코드;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String next = new Scanner(System.in).next();
+
+        char c = next.charAt(0);
+        System.out.println((int)c);
+    }
+}
