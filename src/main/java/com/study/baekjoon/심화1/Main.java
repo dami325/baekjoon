1489cee6eaaee46a7103b0a4b10bd874c668b004
Judalm Park (dami) <wnekfa1004@naver.com>
2024-01-08 22:33:54 +0900

    심화1

--- src/main/java/com/study/baekjoon/심화1/Main.java
+++ src/main/java/com/study/baekjoon/심화1/Main.java
@@ -2,6 +2,14 @@ package com.study.baekjoon.심화1;
 
 public class Main {
     public static void main(String[] args) {
+        char A = 'A';
+        char a = 'a';
 
+        char c = 91;
+
+        System.out.println((int) A);
+        System.out.println((int) a);
+
+        System.out.println(c);
     }
 }
