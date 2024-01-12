35d584f296393853e13df86379cc6a1acd7f5bb1
Judalm Park (dami) <wnekfa1004@naver.com>
2024-01-13 02:21:10 +0900

    2차원배열

--- src/main/java/com/study/baekjoon/배열2차원/세로읽기/Main.java
+++ src/main/java/com/study/baekjoon/배열2차원/세로읽기/Main.java
@@ -2,13 +2,25 @@ package com.study.baekjoon.배열2차원.세로읽기;
 
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
+        BufferedReader br = new BufferedReader(new java.io.InputStreamReader(System.in));
+        String[][] arr = new String[5][15];
 
+        for (int i = 0; i < 5; i++) {
+            String line = br.readLine();
+            for(int j = 0; j < line.length(); j++) {
+                arr[i][j] = String.valueOf(line.charAt(j));
+            }
+        }
+        String result = "";
+        for (int j = 0; j < 15; j++) {
+            for (int i = 0; i < 5; i++) {
+                result += arr[i][j] == null ? "" : arr[i][j];
+            }
+        }
+        System.out.println(result);
     }
 }
