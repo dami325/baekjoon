1d2781f27ffec3ee36ea0422fc9a578135b305e9
Judalm Park (dami) <wnekfa1004@naver.com>
2024-01-27 15:06:20 +0900

    기하 직사각형과 삼각형

---
+++ src/main/java/com/study/baekjoon/기하_직사각형과_삼각형/네번째점/Main.java
@@ -0,0 +1,35 @@
+package com.study.baekjoon.기하_직사각형과_삼각형.네번째점;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+
+        List<Integer> x = new ArrayList<>();
+        List<Integer> y = new ArrayList<>();
+
+        for (int i = 1; i <= 6; i++) {
+            int num = sc.nextInt();
+            if (i % 2 == 0) {
+                if (y.contains(num)) {
+                    y.remove(Integer.valueOf(num));
+                }else {
+                    y.add(num);
+                }
+            } else {
+                if (x.contains(num)) {
+                    x.remove(Integer.valueOf(num));
+                }else {
+                    x.add(num);
+                }
+            }
+        }
+
+        System.out.println(x.get(0) + " " + y.get(0));
+
+    }
+}
---
+++ src/main/java/com/study/baekjoon/기하_직사각형과_삼각형/직사각형/Main.java
@@ -0,0 +1,11 @@
+package com.study.baekjoon.기하_직사각형과_삼각형.직사각형;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println(sc.nextInt() * sc.nextInt());
+    }
+}
