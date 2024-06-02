84155fef2889c77d39e6905ae553dd7bd4b483a7
Judalm Park (dami) <wnekfa1004@naver.com>
2024-06-02 21:52:43 +0900

    [feat] 명령 프롬프트

--- src/main/java/com/study/baekjoon/Main.java
+++ src/main/java/com/study/baekjoon/Main.java
@@ -1,35 +1,52 @@
 package com.study.baekjoon;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
+
+    private static StringBuilder sb = new StringBuilder();
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
 
-        int x = sc.nextInt();
-        int y = sc.nextInt();
-        int w = sc.nextInt();
-        int h = sc.nextInt();
+            int N = Integer.parseInt(br.readLine());
 
-        w -= x;
-        h -= y;
+            String[] strArr = new String[N];
 
-        List<Integer> list = new ArrayList<>();
-        list.add(x);
-        list.add(y);
-        list.add(w);
-        list.add(h);
+            for (int i = 0; i < N; i++) {
+                strArr[i] = br.readLine();
+            }
 
-        int min = x;
-        for (int i = 0; i < 4; i++) {
-            Integer integer = list.get(i);
-            if(integer.compareTo(min) < 0) {
-                min = integer;
+            if (strArr.length == 1) {
+                System.out.println(strArr[0]);
+                return;
+            }
+            for (int i = 0; i < strArr.length-1; i++) {
+                if(i == 0){
+                    strCompare(strArr[i], strArr[i + 1]);
+                }
+                strCompare(sb.toString(), strArr[i + 1]);
+            }
+
+            System.out.println(sb.toString());
+
+        } catch (IOException e) {
+
+        }
+
+    }
+
+    private static String strCompare(String str1, String str2) {
+        sb.setLength(0);
+
+        for (int i = 0; i < str1.length(); i++) {
+            if(str1.charAt(i) == str2.charAt(i)){
+                sb.append(str1.charAt(i));
+            } else {
+                sb.append("?");
             }
         }
-        System.out.println(min);
-
+        return sb.toString();
     }
 }
