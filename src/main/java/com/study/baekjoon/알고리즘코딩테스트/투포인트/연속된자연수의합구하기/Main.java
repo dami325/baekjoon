96541781187be93826b817b96fbdf2cfcc78af02
dami325 <wnekfa1004@naver.com>
2024-06-15 20:19:37 +0900

    소수구하기

---
+++ src/main/java/com/study/baekjoon/알고리즘코딩테스트/투포인트/연속된자연수의합구하기/Main.java
@@ -0,0 +1,30 @@
+package com.study.baekjoon.알고리즘코딩테스트.투포인트.연속된자연수의합구하기;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+
+        int start = 1;
+        int end = 1;
+        int sum = 1;
+        int count = 1;
+
+        while (end != N) {
+            if (sum == N) {
+                count++;
+                end++;
+                sum += end;
+            } else if (sum > N) {
+                sum -= start;
+                start++;
+            } else if (sum < N) {
+                end ++;
+                sum += end;
+            }
+        }
+        System.out.println(count);
+    }
+}
