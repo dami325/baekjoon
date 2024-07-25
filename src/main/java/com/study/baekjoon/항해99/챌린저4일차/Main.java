cc1960841df48440f18ec8a2301f9720927cec51
Judalm Park (dami) <wnekfa1004@naver.com>
2024-07-25 22:03:25 +0900

    [항해99] 카카오 기출 - 문자열 압축

--- src/main/java/com/study/baekjoon/항해99/챌린저4일차/Main.java
+++ src/main/java/com/study/baekjoon/항해99/챌린저4일차/Main.java
@@ -1,32 +1,49 @@
 package com.study.baekjoon.항해99.챌린저4일차;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.ArrayDeque;
+import java.util.Queue;
 
 public class Main {
     public static void main(String[] args) {
         class Solution {
 
             public int solution(String s) {
-                int answer = 0;
 
-                char[] charArray = s.toCharArray();
+                int length = s.length();
 
-                List<Integer> result = new ArrayList<>();
 
-                for (int i = 0; i < s.length(); i++) {
-
-                    for (int j = 0; j < s.length(); j++) {
-                        s.substring(j, j + 1);
+                for (int size = 1; size <= s.length(); size++) {
+                    Queue<String> q = new ArrayDeque<>();
+                    for (int j = 0; j <= s.length() - size; j += size) {
+                        String substring = s.substring(j, j + size);
+                        q.offer(substring);
                     }
 
-                }
+                    String result = s;
+                    while (!q.isEmpty()) {
+                        int count = 1;
+                        String left = q.poll();
+                        while (left.equals(q.peek())) {
+                            left = q.poll();
+                            count++;
+                        }
+                        if (count > 1) {
+                            String replace = "";
+                            for (int i = 0; i < count; i++) {
+                                replace += left;
+                            }
+                            result = result.replaceFirst(replace, count + left);
+                        }
+                    }
 
-                return answer;
+                    length = length > result.length() ? result.length() : length;
+
+                }
+                return length;
             }
 
         }
 
-        System.out.println(new Solution().solution("aabbaccc"));
+        System.out.println(new Solution().solution("abcabcabcabcdededededede"));
     }
 }
